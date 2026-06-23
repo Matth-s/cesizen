42ea419d149c1c08e3bf6d7662b0ff791eb797c4
Matthieu senosiain <senosiain.matthieu@yaoo.fr>
2026-06-23 23:29:48 +0200

    Integration 13

--- backoffice/src/lib/api-client.ts
+++ backoffice/src/lib/api-client.ts
@@ -7,7 +7,7 @@ export const setCsrfToken = (token: string) => {
 };
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3000/api',
+  baseURL: import.meta.env.VITE_PUBLIC_API_ROUTE!,
   withCredentials: true,
 });
 
