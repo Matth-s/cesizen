36242c73786240e0e4bf61c31a088c6b4ff22688
Matthieu senosiain <senosiain.matthieu@yaoo.fr>
2026-06-28 11:52:30 +0200

    csrf

--- backoffice/playwright.config.ts
+++ backoffice/playwright.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './src/__test__/e2e',
 
+  timeout: 30000,
+
+  expect: {
+    timeout: 20000,
+  },
+
   use: {
     baseURL: 'http://127.0.0.1:5173',
     headless: true,
