97503679f4df72911d1bc69eefd794fbc1a7511e
Matthieu senosiain <senosiain.matthieu@yaoo.fr>
2026-06-28 10:51:19 +0200

    ajout test e2e

--- backend/prisma/e2e-seed.ts
+++ backend/prisma/e2e-seed.ts
@@ -2,8 +2,12 @@ import { prisma } from '../src/libs/prisma';
 import { hashPassword } from '../src/libs/bcrypt';
 
 async function seed() {
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: {
+      email: 'admin-endtoend@mail.com',
+    },
+    update: {},
+    create: {
       email: 'admin-endtoend@mail.com',
       password: await hashPassword('password'),
       role: 'ADMIN',
