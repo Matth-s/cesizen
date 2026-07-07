e180932f641ce6e251d44f55b86b0079909fcaab
Matthieu senosiain <senosiain.matthieu@yaoo.fr>
2026-07-07 22:40:11 +0200

    SIEM 7

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,19 @@
 import Fastify from 'fastify';
 import app from './app';
+import pino from 'pino';
+
+const logger = pino({
+  transport: {
+    target: 'pino/file',
+    options: {
+      destination: './logs/app.log',
+      mkdir: true,
+    },
+  },
+});
 
 const server = Fastify({
-  logger: true,
+  logger,
 });
 
 server.register(app);
